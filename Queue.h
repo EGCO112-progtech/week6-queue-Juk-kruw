63b2aa6f70d47d69a04e40ee91b71c3dc404a2d7
Juk-kruw <sawana.thi@student.mahidol.edu>
2025-02-12 08:46:29 +0000

    newest

--- Queue.h
+++ Queue.h
@@ -1,27 +1,51 @@
 
 
-typedef struct {
-	 NodePtr headPtr,tailPtr;
-	int size;
-}Queue;
+typedef struct
+{
+  NodePtr headPtr, tailPtr;
+  int size;
+} Queue;
 
+void enqueue_struct(Queue *q, int x)
+{
+  Node *new_node = (Node *)malloc(sizeof(Node));
+  if (new_node)
+  {
+    new_node->data = x;
+    if (q->size == 0)
+    {
+      q->headPtr = new_node;
+      q->tailPtr = new_node;
+    }
+    else
+    {
 
-void enqueue_struct(Queue* q, int x){
-  Node *new_node=(Node*) malloc(sizeof(Node));
-if(new_node){ 
-  /*Finish enqueue */
- }
+      q->tailPtr->nextPtr = new_node;
+      q->tailPtr = new_node;
+
+      new_node->nextPtr = NULL;
+    }
+
+    q->size += 1;
+  }
 }
 
+int dequeue_struct(Queue *q)
+{
+  if (q->size > 0)
+  {
+    NodePtr t = q->headPtr;
 
-int dequeue_struct(Queue *q){
-   NodePtr t=q->headPtr;
-   if(t){
-   int value= t->data;
-       /*Finish dequeue */
-   return value;
-   }
-   printf("Empty queue");
-   return 0;
+    int value = t->data;
+    q->headPtr = t->nextPtr;
+
+    if (q->headPtr == NULL)
+      q->tailPtr == NULL;
+    free(t);
+    q->size -= 1;
+    return value;
+  }
+  else
+    printf("Empty queue \n");
+  return 0;
 }
-
--- main.c
+++ main.c
@@ -5,27 +5,32 @@
 
 #include "Queue.h"
 
-int main(int argc , char **argv) {
-  NodePtr headPtr=NULL;
-   NodePtr tailPtr=NULL;
-/* For struct Queue
-  Queue  q;
-   q. headPtr=NULL;
-   q.tailPtr=NULL;
-   q.size=0;
-*/
-   int i,x;
-   
+int main(int argc, char **argv)
+{ Queue qq;
 
- for(i=1;i<argc;i++){
-        if(strcmp(argv[i],"x")==0){
-            x=dequeue(&headPtr,&tailPtr);
-            printf("dequeing %d\n",x);
-        }
-        else {
-       enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
-           
-        }
- }
+  NodePtr headPtr = NULL;
+  NodePtr tailPtr = NULL;
+  /* For struct Queue
+    Queue  q;
+     q. headPtr=NULL;
+     q.tailPtr=NULL;
+     q.size=0;
+  */
+  int i, x;
+  qq.size = 0;
+  for (i = 1; i < argc; i++)
+  {
+    if (strcmp(argv[i], "x") == 0)
+    {
+      x = dequeue_struct(&qq);
+       
+      if(x!=0)printf("dequeing %d\n", x);
+    }
+    else
+    {
+      enqueue_struct(&qq, atoi(argv[i]));
+      
+    }
+  }
   return 0;
 }
